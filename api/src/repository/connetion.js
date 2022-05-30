1016a395017cd3cb4fd23abea1ffffc375bdec35
LaysSiqueira <siqueiralays2004@gmail.com>
2022-05-30 11:56:16 -0300

    primeiro push

---
+++ api/src/repository/connetion.js
@@ -0,0 +1,9 @@
+import mysql from 'mysql/promise'
+
+const con = await mysql.creat connection({
+    host: proess.env.mysql_host
+    user: process.env.mysql_user
+    password: process .env.mysql_paasword
+    database: process .env.mysql_
+
+})
